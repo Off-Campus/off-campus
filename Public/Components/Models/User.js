493e4f052818a98231024b399929d4bdcb6aae0e
Lionel Lewis <lionellewis815@gmail.com>
2021-10-09 09:36:19 -0500

    Update User.js

--- Public/Components/Models/User.js
+++ Public/Components/Models/User.js
@@ -1,26 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    firstname: {
-        type: String,
-        required: true
-    },
-    lastname: {
-        type: String,
-        required: true
-    },
-    schoolname: {
-        type: String,
-        required: false
-    },
+const UserSchema = new mongoose.Schema(
+  {
     username: {
+      type: String,
+      required: true,
+    },
+    email: {
         type: String,
         required: true
     },
     password: {
-        type: String,
-        required: true
-    }
-}, {timestamps: true});
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
