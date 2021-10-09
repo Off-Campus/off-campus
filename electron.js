e48769b5ff4307dacc6eb98cb90d7d139e5d079d
Lionel Lewis <lionellewis815@gmail.com>
2021-10-09 08:22:27 -0500

    Update electron.js

--- electron.js
+++ electron.js
@@ -14,9 +14,6 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:3000')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
