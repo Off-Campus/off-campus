5a6b0b5476f914bae44ea76e51ba2dd2d9dc1d24
Lionel Lewis <lionellewis815@gmail.com>
2021-10-09 08:47:49 -0500

    Create User.js

---
+++ Public/Components/Models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    schoolname: {
+        type: String,
+        required: false
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const User = mongoose.model('User', UserSchema);
